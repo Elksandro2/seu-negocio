f7900e7928ebccf370035c4e42b454badc7edca0
Elksandro2 <silvaelksandro@gmail.com>
2025-11-17 22:45:27 -0300

    feat: adicionando código qr para pagamentos via pix

--- src/routes/Cart/index.jsx
+++ src/routes/Cart/index.jsx
@@ -6,7 +6,9 @@ import Loading from '../../components/Loading';
 import MessagePopUp from '../../components/MessagePopUp';
 import styles from './styles.module.css';
 
-import CartItemRow from '../../components/CartItemRow'; 
+import CartItemRow from '../../components/CartItemRow';
+import { Pix } from '@mui/icons-material';
+import { BiCopy } from 'react-icons/bi';
 
 export default function Cart() {
     const [cartItems, setCartItems] = useState([]);
@@ -14,11 +16,15 @@ export default function Cart() {
     const [showMessagePopUp, setShowMessagePopUp] = useState(false);
     const [popUpMessage, setPopUpMessage] = useState('');
     const [severity, setSeverity] = useState('error');
-    
+    const [showQrCode, setShowQrCode] = useState(false);
+    const [copySuccess, setCopySuccess] = useState(false);
+
     const navigate = useNavigate();
     const cartService = new CartService();
-    const { user } = useContext(AuthContext); 
-    
+    const { user } = useContext(AuthContext);
+
+    const pixKey = "d39be51e-5ab8-425c-b68d-cdb0dc30827c";
+
     const total = cartItems.reduce((sum, item) => sum + item.subtotal, 0);
 
     const fetchCart = async () => {
@@ -28,7 +34,7 @@ export default function Cart() {
         }
 
         const result = await cartService.getMyCart();
-        
+
         if (result.success) {
             setCartItems(result.data);
         } else {
@@ -47,9 +53,9 @@ export default function Cart() {
             await handleRemoveItem(itemId);
             return;
         }
-        
+
         const result = await cartService.updateItemQuantity(itemId, newQuantity);
-        
+
         if (result.success) {
             setCartItems(result.data);
         } else {
@@ -62,25 +68,43 @@ export default function Cart() {
         if (!window.confirm("Remover item do carrinho?")) {
             return;
         }
-        
+
         const result = await cartService.removeItem(itemId);
 
         if (result.success) {
             setPopUpMessage("Item removido.");
             setSeverity('success');
             setShowMessagePopUp(true);
-            fetchCart(); 
+            fetchCart();
         } else {
             setPopUpMessage(result.message || "Falha ao remover item.");
             setShowMessagePopUp(true);
         }
     };
 
+    const handleCopy = async () => {
+        try {
+            await navigator.clipboard.writeText(pixKey);
+            
+            setCopySuccess(true);
+            setPopUpMessage('Chave Pix copiada para a área de transferência!');
+            setSeverity('success');
+            setShowMessagePopUp(true); 
+        } catch (err) {
+            setPopUpMessage('Falha ao copiar a chave Pix.');
+            setSeverity('error');
+            setShowMessagePopUp(true);
+            setCopySuccess(false);
+        }
+        
+        setTimeout(() => setCopySuccess(false), 2000); 
+    };
+
 
     if (isLoading) {
         return <Loading />;
     }
-    
+
     if (cartItems.length === 0) {
         return (
             <div className="no-data-container">
@@ -92,7 +116,7 @@ export default function Cart() {
             </div>
         );
     }
-    
+
     return (
         <div className={styles.cartContainer}>
             <h2 className="category-title">Seu Carrinho de Compras</h2>
@@ -100,15 +124,15 @@ export default function Cart() {
             <div className={styles.cartGrid}>
                 <div className={styles.itemsList}>
                     {cartItems.map(item => (
-                        <CartItemRow 
-                            key={item.id} 
-                            cartItem={item} 
+                        <CartItemRow
+                            key={item.id}
+                            cartItem={item}
                             onUpdateQuantity={handleUpdateQuantity}
                             onRemove={handleRemoveItem}
                         />
                     ))}
                 </div>
-                
+
                 <div className={styles.cartSummary}>
                     <h2>Resumo</h2>
                     <div className={styles.summaryItem}>
@@ -116,13 +140,29 @@ export default function Cart() {
                         <span>R$ {total.toFixed(2)}</span>
                     </div>
                     <p className={styles.shippingNote}>Não perca tempo e garanta já o seu pedido!</p>
-                    
-                    <button className={styles.checkoutButton}>
+                    <p className={styles.textPix}><Pix className={styles.pix} fontSize='medium' />Pagamento via Pix</p>
+
+                    <button className={styles.checkoutButton} onClick={() => setShowQrCode(true)}>
                         Finalizar Pedido (R$ {total.toFixed(2)})
                     </button>
+
+                    {showQrCode && (
+                        <div className={styles.qrCode}>
+                            <img src="qrcode.png" alt="QRCode" />
+                            <div className={styles.keyBox}>
+                                <span className={styles.keyText}>{pixKey}</span>
+                                <button
+                                    onClick={handleCopy}
+                                    className={styles.copyButton}
+                                >
+                                    {copySuccess ? 'Copiado!' : <BiCopy size={18} />}
+                                </button>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
-            
+
             {showMessagePopUp && (
                 <MessagePopUp message={popUpMessage} showPopUp={setShowMessagePopUp} severity={severity} />
             )}
